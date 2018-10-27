97dc6585a437b767512eed8d0d8d3c7c2531fd00
UM #2521 <2521um@gmail.com>
2018-10-27 21:41:10 +0900

    1. 181026 HCI 수업에서 발생한 에러를 해결했습니다. 1) 한 번에 입력한 여러줄의 메세지는 첫줄만 포함되는 에러 2) ']' 문자가 있는 메세지가 있으면 index 에러
    2. 유저가 수정할 상수 2가지를 최상단 위치로 올렸습니다.
    9, 10번줄만 수정하면 됩니다.
    
    3. 주석과 변수명을 수정했습니다.

--- import_intents.py
+++ import_intents.py
@@ -1,5 +1,19 @@
 # 원본 출처: http://ndb796.tistory.com/127?category=1013435
 import openpyxl
+import re
+
+''' 코드 실행 전 아래 2개 상수를 자신의 환경에 맞게 수정합니다.
+    EXCEL_FILE_NAME: json 파일로 변환한 엑셀 파일 이름을 넣어줍니다.
+    MY_NAME: 아바타를 만들 대상의 카카오톡 이름을 넣어줍니다.
+'''
+EXCEL_FILE_NAME = "test.xlsx"
+MY_NAME = "엄민식"
+
+
+''' 메세지 첫줄 형식이거나 날짜 형식인지 검사하기 위한 정규식입니다.
+'''
+MESSAGE_FORMAT = re.compile(r'\[.*\] \[.*\].*')
+DATE_FORMAT = re.compile(r'---------------.*--------------')
 
 
 class Intent:
@@ -24,43 +38,47 @@ if __name__ == "__main__":
     """ 카카오톡 대화 내용을 저장해둔 엑셀 파일을 열어서 데이터를 가져와
         Dialogflow에 넣을 수 있는 형태로 정보를 가공합니다.        
     """
-    wb = openpyxl.load_workbook('test.xlsx')
+    wb = openpyxl.load_workbook(EXCEL_FILE_NAME)
     ws = wb.active
 
     # Intent 객체를 만들기 전 질문/대답들을 모두 담아두는 리스트를 만듭니다.
     chat_list = []
-    # 아바타를 만들 대상의 카카오톡 이름을 넣어줍니다.
-    MY_NAME = "엄민식"
 
     # 엑셀 내부 데이터를 쭉 돌아가며 처리를 합니다.
     for r in ws.rows:
 
         # 빈줄은 넘어갑니다.
-        if r is None:
+        if r[0].value is None:
             continue
 
-        # 데이터를 받아 ] 단위로 텍스트를 쪼개 저장합니다.
-        data = r[0].value.split(']')
+        # 숫자로만 된 줄은 int 형으로 저장되므로 전부 str 형으로 바꿔줍니다.
+        value = str(r[0].value)
 
-        # 채팅 메세지 이외의 텍스트는 ']' 가 없기에 쪼개지 않고 크기가 1인 리스트로 반환됩니다.
-        # 채팅 메세지 이외의 텍스트는 처리를 하지 않고 버리고 넘어갑니다.
-        if len(data) == 1:
-            continue
+        if MESSAGE_FORMAT.match(value):
+            # 메세지 첫줄 형식과 일치하는지 확인합니다.
+            # 데이터를 받아 ] 단위로 텍스트를 쪼개 저장합니다.
+            chat = value.split(']')
 
-        # 메세지 보낸 사람을 저장합니다.
-        message_sender = data[0][1:].strip()
-        # 대화 텍스트를 저장합니다.
-        user_message = data[2].strip()
+            # 메세지 보낸 사람을 저장합니다.
+            chat_sender = chat[0][1:].strip()
+            # 대화 텍스트를 저장합니다.
+            chat_text = chat[2].strip()
 
-        if not chat_list:
-            # chat_list 에 아직 담긴 게 없으면 그냥 넣어줍니다.
-            chat_list.append([message_sender, user_message])
-        else:
-            # 한 사람이 계속해서 말하면 뒤에 붙여주고 다른 사람 말이면 새로 메세지를 넣어줍니다.
-            if chat_list[-1][0] == message_sender:
-                chat_list[-1][1] += " " + user_message
+            if not chat_list:
+                # chat_list 에 아직 담긴 게 없으면 그냥 넣어줍니다.
+                chat_list.append([chat_sender, chat_text])
             else:
-                chat_list.append([message_sender, user_message])
+                # 한 사람이 계속해서 말하면 뒤에 붙여주고 다른 사람 말이면 새로 메세지를 넣어줍니다.
+                if chat_list[-1][0] == chat_sender:
+                    chat_list[-1][1] += " " + chat_text
+                else:
+                    chat_list.append([chat_sender, chat_text])
+        elif not DATE_FORMAT.match(value):
+            # 메세지 형식도 아니고 날짜 형식도 아닌 경우는 2가지
+            # 1. 제일 처음에 --님과의 대화와 저장한 날짜: chat_list가 비어있으므로 조건 확인 후 넘어감.
+            # 2. 여러줄의 메세지에서 2번째 이상 줄: 마지막 메세지에 개행과 함께 내용 연결
+            if chat_list:
+                chat_list[-1][1] += "\n" + value
 
     wb.close()
 
@@ -84,7 +102,7 @@ if __name__ == "__main__":
     for c in Intents:
         print(str(c))
 
-    print('총 ', len(Intents), '개의 질문-대답이 존재합니다.')
+    print('총 ', len(Intents), '개의 Intent가 생성되었습니다.')
 
     """ 이후 코드는 원 출처(코드 최상단 주석 참고)의 코드와 동일합니다.
         intent 객체들로 저장된 데이터를 데이터 형식에 맞추어 텍스트를 만들고
